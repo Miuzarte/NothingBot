db2d9fbf489da72505ddee30b9ec407e68937f10
Miuzarte <982809597@qq.com>
2023-07-14 09:34:34 +0800

    scene support

--- corpus/corpus.go
+++ corpus/corpus.go
@@ -18,23 +18,39 @@ var (
 )
 
 func initOnRegex() { //注册用户语料库
-	for k := range v.AllSettings() {
+	logrus.Infof("[corpus] 语料库找到 %d 条", len(v.GetStringMap("corpus")))
+	counts := 0
+	for k := range v.GetStringMap("corpus") {
+		counts++
 		c := k
-		fmt.Printf("%s.regexp: %T\n", c, v.Get(fmt.Sprintf("%s.reply", c)))
-		engine.OnRegex(v.GetString(fmt.Sprintf("%s.regexp", c))).Handle(func(ctx *zero.Ctx) {
-			time.Sleep(time.Second * time.Duration(v.GetInt64(fmt.Sprintf("%s.delay", c))))
-			switch v.Get(fmt.Sprintf("%s.reply", c)).(type) {
+		logrus.Infof("[corpus] Type of corpus.%s.reply: %T", c, v.Get(fmt.Sprintf("corpus.%s.reply", c)))
+		scene := func(ctx *zero.Ctx) bool {
+			switch v.GetString(fmt.Sprintf("corpus.%s.scene", c)) {
+			case "a": // 全部
+				return true
+			case "g": // 群
+				return v.GetString(fmt.Sprintf("corpus.%s.scene", c)) == "g" && ctx.Event.DetailType == "group"
+			case "p": // 私聊
+				return v.GetString(fmt.Sprintf("corpus.%s.scene", c)) == "p" && ctx.Event.DetailType == "private"
+			default:
+				return false
+			}
+		}
+		engine.OnRegex(v.GetString(fmt.Sprintf("corpus.%s.regexp", c)), scene).Handle(func(ctx *zero.Ctx) {
+			time.Sleep(time.Second * time.Duration(v.GetInt64(fmt.Sprintf("corpus.%s.delay", c))))
+			switch v.Get(fmt.Sprintf("corpus.%s.reply", c)).(type) {
 			case string:
-				ctx.SendChain(message.Text(v.GetString(fmt.Sprintf("%s.reply", c))))
+				ctx.Send(message.Text(v.GetString(fmt.Sprintf("corpus.%s.reply", c))))
 			case []string, []any:
-				ctx.SendChain(message.Text(v.GetString(fmt.Sprintf("%s.reply.%d", c, rand.Intn(len(v.GetStringSlice(fmt.Sprintf("%s.reply", c))))))))
+				ctx.Send(message.Text(v.GetString(fmt.Sprintf("corpus.%s.reply.%d", c, rand.Intn(len(v.GetStringSlice(fmt.Sprintf("%s.reply", c))))))))
 			}
 		})
 	}
+	logrus.Infof("[corpus] 语料库注册 %d 条", counts)
 }
 
 func init() {
-	logrus.Infoln("[main] 读取语料库配置")
+	logrus.Infoln("[corpus] 读取语料库配置")
 	v.SetConfigName("config")
 	v.SetConfigType("yaml")
 	v.AddConfigPath(".")
